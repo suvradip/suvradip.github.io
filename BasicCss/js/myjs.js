cdd275768425fbf222967f147db4b41ec80a14c2
suvradip <suvradip@fusioncharts.com>
2015-07-16 11:13:31 +0530

    Update myjs.js

--- BasicCss/js/myjs.js
+++ BasicCss/js/myjs.js
@@ -1,27 +1,29 @@
-var data=[];
+
 var int1, int2, int3;
 		 
-
-
-function reset()
+function tabToggle(x)
 {
-//reset of text boxes.	
-document.getElementById("name").value="";
-document.getElementById("email").value="";
-document.getElementById("country").selectedIndex=0;
-document.getElementById("address").value="";
-
-//reset of radio buttons.
- var ele = document.getElementsByName("sex");
-   for(var i=0;i<ele.length;i++)
-      ele[i].checked = false;
-
-//reset of checkboxes
-document.getElementById("cb1").checked = false;
-document.getElementById("cb2").checked = false;
-document.getElementById("cb3").checked = false;
-
-}
+	
+	if(x==1)
+	{	
+	document.getElementById("x1").style.zIndex = "0";
+	document.getElementById("x2").style.zIndex = "1";
+	document.getElementById("tab1_h2").style.backgroundColor = "#FFFFFF";
+	document.getElementById("tab2_h2").style.backgroundColor = "#E4EFFF";
+	document.getElementById("tab1_h2_a").style.color = "#717171";
+	document.getElementById("tab2_h2_a").style.color = "#09569d";
+	}
+	else
+	{
+	document.getElementById("x1").style.zIndex = "1";
+	document.getElementById("x2").style.zIndex = "0";
+	document.getElementById("tab1_h2").style.backgroundColor = "#E4EFFF";
+	document.getElementById("tab2_h2").style.backgroundColor = "#FFFFFF";
+	document.getElementById("tab1_h2_a").style.color = "#09569d";
+	document.getElementById("tab2_h2_a").style.color = "#717171";
+	
+	}
+}	
 
 
 function checkboxValue(cb1,cb2,cb3)
@@ -64,57 +66,40 @@ function radioCheckedValue(rname)
 function getData()
 {
 
-//gets the all value from text boxes	
-var n=document.getElementById("name").value;
-var e=document.getElementById("email").value;
-//var c=document.getElementById("country").value;
-var ele = document.getElementById("country");
-var c = ele.options[ele.selectedIndex].text;
+//checking textboxe values
+if(document.getElementById("name").value!="" && document.getElementById("email").value!=""  && document.getElementById("country").selectedIndex!=0 && document.getElementById("address").value!=""  )
+		{
 
-var a=document.getElementById("address").value;
-var cb1=document.getElementById("cb1");
-var cb2=document.getElementById("cb2");
-var cb3=document.getElementById("cb3");
+			//gets the all value from text boxes	
+			var n=document.getElementById("name").value;
+			var e=document.getElementById("email").value;
+			var ele = document.getElementById("country");
+			var c = ele.options[ele.selectedIndex].text;
 
-checkboxValue(cb1,cb2,cb3);
+			var a=document.getElementById("address").value;
+			var cb1=document.getElementById("cb1");
+			var cb2=document.getElementById("cb2");
+			var cb3=document.getElementById("cb3");
 
-var gender =  radioCheckedValue("sex");
-//storing this data in Jason 
-	data.push({
-						"name":n,
-						"email":e,
-						"country":c,
-						"address":a,
-						"sex":gender,
-						"interest1":int1,
-						"interest2":int2,
-						"interest3":int3
+			checkboxValue(cb1,cb2,cb3);
 
-				   });
+			var gender =  radioCheckedValue("sex");
+			//storing this data in Jason 
 
-	//console.log(data);
-}
+				var data=({
+									"name":n,
+									"email":e,
+									"country":c,
+									"address":a,
+									"sex":gender,
+									"interest1":int1,
+									"interest2":int2,
+									"interest3":int3
+
+							   });
+
+				console.log(JSON.stringify(data));
+		}
 
 
-// to view all data, run this function from console eg. showdata();
-
-function showdata()
-{
-for(var i in data)
-{
-	console.log("--------------------")
-	console.log("Data Object "+ i );
-	console.log("--------------------")
-	console.log("Name is : "+data[i].name);
-	console.log("Email is : "+data[i].email);
-	console.log("Country is : "+data[i].country);
-	console.log("Address is : "+data[i].address);
-	console.log("sex is : "+data[i].sex);
-	console.log("Interest 1 is : "+data[i].interest1);
-	console.log("Interest 2 is : "+data[i].interest2);
-	console.log("Interest 3 is : "+data[i].interest3);
-	
-
-}
-	
 }
